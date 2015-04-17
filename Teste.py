56041cc4606c8a829ab76de6a62341c887788ab8
Joao Paulo Taylor Ienczak Zanette <profissional.jptiz@gmail.com>
2015-04-17 18:09:00 -0300

    Musiquinha + SFX tocando. Muitas vezes tocar SFX = error segfault

--- Teste.py
+++ Teste.py
@@ -1,4 +1,5 @@
 import pyglet
+from pyglet.image.codecs.png import PNGImageDecoder
 from pyglet.window import mouse
 from pyglet.window import key
 
@@ -8,20 +9,22 @@ window.set_size(640, 480)
 window.set_mouse_visible(False)
 
 # Soh declarando azibagem
-image = pyglet.image.load('img/xicrinha.jpg')
+image = pyglet.image.load('img/xicrinha.png', decoder=PNGImageDecoder())
 image.opacity = .5
 image.x = 320 - image.width/2
 image.y = 240 - image.height/2
-image2 = pyglet.image.load('img/frame.PNG')
+image2 = pyglet.image.load('img/frame.PNG', decoder=PNGImageDecoder())
 frame = image2.get_region(0, 0, 109, 321)
 xas = 0
+porrada = pyglet.resource.media('snd/punchVladimir.wav', streaming=False)
+
 def sumx():
 	global xas
 	xas = (xas + 1) % 5
 
 
 # Testes com muzisquinha
-music = pyglet.resource.media('rola.mp3')
+music = pyglet.resource.media('bgm/Johnny.mp3')
 music.play()
 
 # sei la o que eh "dx" e "dy"
@@ -40,7 +43,9 @@ def on_mouse_press(x, y, button, modifiers):
 # Aqui ta legal
 @window.event
 def on_key_press(symbol, modifiers):
-	if symbol == key.LEFT: image.x += 10
+	if symbol == key.LEFT:
+		image.x += 10
+		porrada.play()
 
 # Tudo funciona ok
 # soh que a droga do pyglet considera o canto esquerdo-abaixo como ponto
