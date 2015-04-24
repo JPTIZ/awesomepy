4bb821c25044dd099c6be370a495c9f007a953b1
Joao Paulo Taylor Ienczak Zanette <profissional.jptiz@gmail.com>
2015-04-24 19:35:33 -0300

    Muitos progressos (classes novas -- aka sprite, métodos novos, reconhecimento de teclas, tela de título pronta, tela de estágio 1 iniciada, openal utilizado no lugar do driver bugado

--- game.py
+++ game.py
@@ -1,3 +1,9 @@
+import pyglet
+from pyglet import clock
+from pyglet.gl import *
+from pyglet.window import key
+from pyglet.image.codecs.png import PNGImageDecoder
+
 # Controle de Frame-rating
 FPS = 60
 frame_count = 0
@@ -5,3 +11,79 @@ frame_count = 0
 # Janela
 TITLE = "Steroids"
 SIZE = (640, 480)
+
+scene = None
+fps_display = clock.ClockDisplay()
+keys = []
+
+# -----------------------------------------------------------------------------
+# Material para estruturação do jogo
+# -----------------------------------------------------------------------------
+
+
+class Window(pyglet.window.Window):
+    def __init__(self, width, height, *args, **kwargs):
+        super(Window, self).__init__(width=width, height=height,
+                                     *args, **kwargs)
+        self.x_proportion = 1
+        self.y_proportion = 1
+
+    def on_draw(self):
+        self.clear()
+
+    def on_close(self):
+        self.close()
+        exit(0)
+
+    def on_key_press(self, symbol, modifiers):
+        keys.append(str(symbol))
+        scene.on_key_press(symbol, modifiers)
+
+    def on_key_release(self, symbol, modifiers):
+        keys.remove(str(symbol))
+
+    def on_resize(self, width, height):
+        self.x_proportion = width/SIZE[0]
+        self.y_proportion = height/SIZE[1]
+        super(Window, self).on_resize(width, height)
+        self.on_draw()
+
+
+class SoundManager:
+    def __init__(self):
+        self.player = pyglet.media.Player()
+        self.player.eos_action = pyglet.media.Player.EOS_LOOP
+
+    def play(self, resource):
+        self.player.next()
+        self.player.queue(resource)
+        self.player.play()
+
+
+class Sprite:
+    def __init__(self, filename):
+        self.bitmap = pyglet.image.load(filename, decoder=PNGImageDecoder())
+        self.x = 0
+        self.y = 0
+        self.angle = 0
+        self.visible = True
+
+    def update(self, window):
+        if not self.visible:
+            return
+        trueX = self.x * window.x_proportion
+        trueY = (SIZE[1] - (self.y+self.bitmap.height)) * window.y_proportion
+        glTranslatef(self.x, self.y, 0)
+        glRotatef(self.angle, 0, 0, 1)
+        self.bitmap.blit(trueX,
+                         trueY,
+                         width=self.bitmap.width*window.x_proportion,
+                         height=self.bitmap.height*window.y_proportion)
+        glRotatef(-self.angle, 0, 0, 1)
+        glTranslatef(-self.x, -self.y, 0)
+
+
+class SceneBase:
+    def on_key_press(self, symbol, modifiers):
+        print("Pressionou tecla")
+        print("Tecla: "+str(symbol))
--- steroids.py
+++ steroids.py
@@ -1,47 +1,102 @@
-# Imports
+# Imports externos
 import pyglet
 from pyglet import clock
+from pyglet.gl import *
+from pyglet.window import key
+# Imports internos
 import game
+from game import Window
+from game import SoundManager
+from game import Sprite
+from game import keys
+
+pyglet.options['audio'] = ('alsa', 'openal', 'silent')
+
+# -----------------------------------------------------------------------------
+# Cenas do jogo
+# -----------------------------------------------------------------------------
 
 
-#-----------------------------------------------------------------------------
-# Material para estruturação do jogo
-#-----------------------------------------------------------------------------
-
-fps_display = clock.ClockDisplay()
-
-class Window(pyglet.window.Window):
-    def on_draw(self):
-        self.clear()
-        fps_display.draw()
-
-    def on_close(self):
-        self.close()
-        exit(0)
-
-class SoundManager:
+class SceneTitle(game.SceneBase):
+    # Tela de título
     def __init__(self):
-        self.player = pyglet.media.Player()
+        self.background = Sprite('img/titleBackground.png')
+        self.logo = Sprite('img/steroidsLogo.png')
+        self.logo.x = 20
+        self.logo.y = 20
+        self.pressStart = Sprite('img/pressStart.png')
+        self.pressStart.x = 320-(self.pressStart.bitmap.width/2)
+        self.pressStart.y = 320
+        self.fadeTime = 120
+        music_player.play(pyglet.media.load('bgm/title.mp3'))
 
-    def play(self, resource):
-        self.player.next()
-        self.player.queue(resource)
-        self.player.play()
+    def update(self):
+        self.background.update(window)
+        self.logo.update(window)
+        self.pressStart.update(window)
+        if self.fadeTime == 0:
+            game.scene = SceneStage1()
+        if self.fadeTime < 120:
+            self.fadeTime -= 1
+            print("Remaining fade delay: "+str(self.fadeTime))
+            if self.fadeTime % 5 == 0:
+                self.pressStart.visible = not self.pressStart.visible
 
-#-----------------------------------------------------------------------------
+    def on_key_press(self, symbol, modifiers):
+        print("Pressou tecla!")
+        if symbol == key.RETURN and self.fadeTime >= 120:
+            self.fadeTime -= 1
+            pyglet.media.load('snd/confirm.wav', streaming=False).play()
+
+
+class SceneStage1(game.SceneBase):
+    # Primeiro estágio
+    def __init__(self):
+        self.background = Sprite('img/titleBackground.png')
+        self.player = Sprite('img/player.png')
+        music_player.play(pyglet.media.load('bgm/stage1.mp3'))
+        print("Iniciou primeiro estágio")
+
+    def update(self):
+        if str(key.RIGHT) in keys:
+            self.player.angle += 1
+        if str(key.LEFT) in keys:
+            self.player.angle -= 1
+        if str(key.UP) in keys:
+            self.player.y -= 1
+        if str(key.DOWN) in keys:
+            self.player.y += 1
+        self.background.update(window)
+        self.player.update(window)
+
+    def on_key_press(self, symbol, modifiers):
+        print("Pressou teclinha")
+        if symbol == key.RIGHT:
+            self.player.x += 1
+        if symbol == key.LEFT:
+            self.player.x -= 1
+        if symbol == key.UP:
+            self.player.y -= 1
+        if symbol == key.DOWN:
+            self.player.y += 1
+
+# -----------------------------------------------------------------------------
 # Processo principal do jogo
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 # 1 - Criação da janela de jogo
-window = Window(game.SIZE[0], game.SIZE[1])
-window.set_caption(game.TITLE)
+window = Window(width=game.SIZE[0], height=game.SIZE[1], resizable=True)
+window.set_caption(game.TITLE + " - Initializing...")
 window.set_mouse_visible(False)
 
 # 2 - Instanciação do que for necessário (elementos globais)
-music_player = SoundManager()
+music_player = SoundManager()                       # Tocador de músicas
+game.scene = SceneTitle()                           # Cena inicial do jogo
 
 # 3 - Definição das configurações principais
 clock.set_fps_limit(game.FPS)
+glEnable(GL_BLEND)                                  # Habilitar canal alpha #1
+glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)   # Habilitar canal alpha #2
 
 # 4 - Definição do loop principal
 while 1:
@@ -50,11 +105,10 @@ while 1:
     for window in pyglet.app.windows:
         window.switch_to()
         window.dispatch_events()
-        window.dispatch_event('on_draw')0
-====================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================
-
-
-+========7        window.flip
-        window.set_caption(game.TITLE + " - " + str(int(clock.get_fps()))+" FPS")
+        game.scene.update()  # Atualização principal da cena atual do jogo
+        window.dispatch_event('on_draw')
+        window.flip()
+        window.set_caption(game.TITLE + " - " +
+                           str(int(clock.get_fps()))+" FPS")
 
     game.frame_count += 1
